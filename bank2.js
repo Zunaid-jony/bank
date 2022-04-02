2fe4348d47dc12b60815ca730621bbdc64fe1c69
Zunaid-jony <jony35666666@gmail.com>
2022-04-02 23:03:55 +0600

    final update

--- bank2.js
+++ bank2.js
@@ -24,11 +24,61 @@ document.getElementById('deposit-button').addEventListener('click', function( ){
     const balanceTotallText = balanceTotall.innerText;
     const previousBalanceTotal = parseFloat(balanceTotallText);
 
-    const newDipositTotal2 = previousBalanceTotal + depositAmoutt ;
+    //const newDipositTotal2 = previousBalanceTotal + previousDepoitA ;
 
-    balanceTotall.innerText = newDipositTotal2;
+    balanceTotall.innerText = previousBalanceTotal + previousDepoitA;
 
 
     depositInput.value = '';
 
+})
+// handle withdraw button
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmountTex = withdrawInput.value;
+    const windowAmount1 = parseFloat( withdrawAmountTex);
+    //console.log(withdrawAmountTex);
+
+
+// update withdraw total 
+
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const withdrawTotalText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(withdrawTotalText);
+    withdrawTotal.innerText = previousWithdrawTotal + windowAmount1 ;
+
+    // update balance withdraw 
+    const balanceTotall = document.getElementById('balance-total');
+    const balanceTotallText = balanceTotall.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotallText);
+
+    //const newDipositTotal2 = previousBalanceTotal + previousDepoitA ;
+
+    balanceTotall.innerText = previousBalanceTotal - windowAmount1;
+    withdrawInput.value = '';
+
+
+    depositInput.value = '';
+
+
+
+
+
+    withdrawInput.value = '';
+    //console.log('abar pais re')
+    //const withdrawInput = document.getElementById('withdraw-input');
+
+
+
+
+
+
+    //const totalwithdrwTex = withdrawInput.value;
+
+
+    //console.log(totalwithdrwTex);
+    // const previousWithdroTotal = parseFloat(totalwithdrwTex);
+    // withdrawInput.innerText = previousWithdroTotal - previousBalanceTotal ;
+
+   
 })
