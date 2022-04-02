32acb6db73d8ecb1a86cf84b66484fcef7aeaac3
Zunaid-jony <jony35666666@gmail.com>
2022-04-02 14:50:44 +0600

    first commit

---
+++ bank.js
@@ -0,0 +1,23 @@
+document.getElementById('login-submit').addEventListener('click',function(){
+    //get user Email field
+   const emalField = document.getElementById('user-email');
+   const userField = emalField.value;
+  
+    //get user password field
+
+    const passeordFiled = document.getElementById('user-password');
+    const userPassword = passeordFiled.value;
+
+    // password === if ..
+
+    if( userField == 'jony12345@gmail.com' && userPassword == 'jony12345'){
+        window.location.href ='banking.html';
+        
+    }
+    else{
+        console.log('invalid user');
+    }
+    
+
+
+})
